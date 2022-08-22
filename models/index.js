eb37d3a2d49e3a9ec71e69a906d72b8a21b8213c
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2022-08-22 19:58:22 -0400

    POST routes

--- models/index.js
+++ models/index.js
@@ -1,4 +1,13 @@
 const User = require('./User');
 const Post = require('./Post');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    forgienKey: 'user_id',
+});
+
 module.exports = { User, Post };
